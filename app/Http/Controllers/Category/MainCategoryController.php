7b74c9453887cc81aa4de80bfca6242c3cc94f61
Saman Farabi <saman.farabi1818@gmail.com>
2023-01-23 20:09:45 +0330

    some changes in composer

--- app/Http/Controllers/Category/MainCategoryController.php
+++ app/Http/Controllers/Category/MainCategoryController.php
@@ -2,19 +2,21 @@
 namespace App\Http\Controllers\Category;
 
 use App\Http\Controllers\Controller;
-use Saman\BarekatElectronicHealth\App\Contract\Repositories\ICategory;
+use Saman\BarekatElectronicHealth\App\Models\Category;
+//use Saman\BarekatElectronicHealth\App\Contract\Repositories\ICategory;
 
 class MainCategoryController extends Controller
 {
-    public ICategory $category;
-
-    public function __construct(ICategory $category)
-    {
-        $this->category = $category;
-    }
+//    public ICategory $category;
+//
+//    public function __construct(ICategory $category)
+//    {
+//        $this->category = $category;
+//    }
 
     public function index()
     {
-        return $this->category->index();
+        //return $this->category->index();
+        return Category::query()->with('allChildren')->select(Category::COLUMN_TITLE,Category::COLUMN_PARENT_ID,Category::COLUMN_IS_ACTIVE)->activeCategory()->get();
     }
 }
