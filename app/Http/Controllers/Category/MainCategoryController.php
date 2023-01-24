578e3e3c90f72bfc1f36bef9f114cc83111a5ffe
Saman Farabi <saman.farabi1818@gmail.com>
2023-01-24 10:39:27 +0330

    some changes related to package

--- app/Http/Controllers/Category/MainCategoryController.php
+++ app/Http/Controllers/Category/MainCategoryController.php
@@ -2,7 +2,6 @@
 namespace App\Http\Controllers\Category;
 
 use App\Http\Controllers\Controller;
-use Saman\BarekatElectronicHealth\App\Models\Category;
 use Saman\BarekatElectronicHealth\App\Contract\Repositories\ICategory;
 
 class MainCategoryController extends Controller
@@ -16,7 +15,6 @@ class MainCategoryController extends Controller
 
     public function index()
     {
-        //return $this->category->index();
-        return Category::query()->with('allChildren')->select(Category::COLUMN_TITLE,Category::COLUMN_PARENT_ID,Category::COLUMN_IS_ACTIVE)->activeCategory()->get();
+        return $this->category->index();
     }
 }
