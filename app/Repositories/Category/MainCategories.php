1ea80db9b0e7507dfffc9262fc41078e72244e21
Saman Farabi <saman.farabi1818@gmail.com>
2023-01-24 12:10:01 +0330

    some changes2

--- app/Repositories/Category/MainCategories.php
+++ app/Repositories/Category/MainCategories.php
@@ -8,6 +8,6 @@ class MainCategories implements ICategory
 {
     public function index()
     {
-        return Category::query()->with('allChildren')->select(Category::COLUMN_TITLE,Category::COLUMN_PARENT_ID,Category::COLUMN_IS_ACTIVE)->activeCategory()->get();
+        return Category::getCategories();
     }
 }
